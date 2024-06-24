53894a1e09380789625688bd1b256ba73f1c9cd3
周哲 <1708834102@qq.com>
2024-06-24 15:53:36 +0800

    init

---
+++ contracts/BananaMarketplace.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.26;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import {BananaNFT} from "./BananaNFT.sol";
+
+contract BananaMarketplace is IERC721Receiver, Ownable {
+    IERC20 public token;
+    BananaNFT public bananaNFT;
+    uint256 public burnRate = 5; // 5%
+    
+    struct Listing {
+        address seller;
+        uint256 price;
+    }
+    
+    mapping(uint256 => Listing) public listings;
+
+    event Listed(uint256 indexed tokenId, address seller, uint256 price);
+    event Purchased(uint256 indexed tokenId, address buyer, uint256 price);
+
+    constructor(IERC20 _token, BananaNFT _nft) Ownable(msg.sender) {
+        token = _token;
+        bananaNFT = _nft;
+    }
+
+    function listNFT(uint256 tokenId, uint256 price) external {
+        require(bananaNFT.ownerOf(tokenId) == msg.sender, "Not owner");
+        require(listings[tokenId].seller == address(0), "Already listed");
+
+        bananaNFT.transferFrom(msg.sender, address(this), tokenId);
+        listings[tokenId] = Listing({seller: msg.sender, price: price});
+
+        emit Listed(tokenId, msg.sender, price);
+    }
+
+    function buyNFT(uint256 tokenId) external {
+        Listing memory listing = listings[tokenId];
+        require(listing.seller != address(0), "Not listed");
+
+        uint256 burnAmount = (listing.price * burnRate) / 100;
+        uint256 sellerAmount = listing.price - burnAmount;
+
+        token.transferFrom(msg.sender, address(this), burnAmount);
+        token.transferFrom(msg.sender, listing.seller, sellerAmount);
+        bananaNFT.transferFrom(address(this), msg.sender, tokenId);
+
+        delete listings[tokenId];
+        emit Purchased(tokenId, msg.sender, listing.price);
+    }
+
+    function onERC721Received(
+        address /*operatorI*/,
+        address /*from*/,
+        uint256 /*tokenId*/,
+        bytes calldata /*data*/
+    ) external pure override returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+    
+    function setBurnRate(uint256 _burnRate) external onlyOwner {
+        require(_burnRate < 20, "Invalid _burnRate");
+        burnRate = _burnRate;
+    }
+
+    function withdrawBurnedTokens() external onlyOwner {
+        uint256 balance = token.balanceOf(address(this));
+        token.transfer(owner(), balance);
+    }
+}
---
+++ contracts/BananaNFT.sol
@@ -0,0 +1,149 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.26;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+
+contract BananaNFT is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+
+    uint256 private _tokenIdCounter;
+
+    IERC20 public token;
+    uint256 public mintPrice = 0.01 ether;
+    uint256 public mintInterval;
+    uint256 public saleEndTime;
+
+    bool public _isSaleActive = false;
+    // 初始化盲盒，等到一定时机可以随机开箱，变成true
+    bool public _revealed = false;
+    string baseURI;
+    // 盲盒图片的meta,json地址，后文会提到
+    string public notRevealedUri;
+
+    mapping(address => uint256) public lastMintTime;
+    mapping(uint256 => uint256) public tokenLevels;
+    mapping(address => bool) public isOnline;
+
+    event Minted(address indexed user, uint256 tokenId, uint256 level);
+
+    constructor(
+        uint256 _mintInterval,
+        IERC20 _token
+    ) ERC721("BananaNFT", "BNFT") Ownable(msg.sender) {
+        mintInterval = _mintInterval;
+        token = _token;
+    }
+
+    function startSale(uint256 duration) external onlyOwner {
+        saleEndTime = block.timestamp + duration;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_ownerOf(tokenId) != address(0), "ERC721Metadata: URI query for nonexistent token");
+        if (_revealed == false) {
+            return notRevealedUri;
+        }
+        string memory base = _baseURI();
+
+        return bytes(base).length > 0 ? string(abi.encodePacked(base, tokenId.toString())) : "";
+    }
+
+    function mysteryBox() external payable {
+        require(_isSaleActive, "Sale is not active");
+        require(block.timestamp < saleEndTime, "Sale ended");
+        require(msg.value == mintPrice, "Incorrect ETH value");
+
+        ++_tokenIdCounter;
+        uint256 tokenId = _tokenIdCounter;
+        _safeMint(msg.sender, tokenId);
+
+        uint256 level = _getRandomLevel();
+        tokenLevels[tokenId] = level;
+
+        emit Minted(msg.sender, tokenId, level);
+    }
+
+    function mint(address user) external {
+        require(
+            block.timestamp >= lastMintTime[user] + mintInterval,
+            "Isnt mint time"
+        );
+        require(isOnline[user], "You are not online");
+        require(block.timestamp < saleEndTime, "Sale ended");
+
+        ++_tokenIdCounter;
+        uint256 tokenId = _tokenIdCounter;
+        lastMintTime[user] = block.timestamp;
+        _safeMint(msg.sender, tokenId);
+
+        uint256 level = _getRandomLevel();
+        tokenLevels[tokenId] = level;
+
+        emit Minted(msg.sender, tokenId, level);
+    }
+
+    function updateOnlineMintTime(address user) external onlyOwner {
+        lastMintTime[user] = block.timestamp;
+        isOnline[user] = true;
+    }
+
+    function updateOutlineMintTime(address user) external onlyOwner{
+        lastMintTime[user] = block.timestamp;
+        isOnline[user] = false;
+    }
+
+     function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+   
+    function flipSaleActive() external onlyOwner {
+        _isSaleActive = !_isSaleActive;
+    }
+   
+    function flipReveal() external onlyOwner {
+        _revealed = !_revealed;
+    }
+   
+    function setMintPrice(uint256 _mintPrice) external onlyOwner {
+        mintPrice = _mintPrice;
+    }
+   
+    function setNotRevealedURI(string memory _notRevealedURI) external onlyOwner {
+        notRevealedUri = _notRevealedURI;
+    }
+   
+    function setBaseURI(string memory _newBaseURI) external onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+    function _getRandomLevel() private view returns (uint256) {
+        uint256 rand = uint256(
+            keccak256(
+                abi.encodePacked(
+                    block.timestamp,
+                    msg.sender,
+                    _tokenIdCounter
+                )
+            )
+        );
+        if (rand % 100 < 5) {
+            // 5% chance for high level
+            return 3;
+        } else if (rand % 100 < 20) {
+            // 15% chance for mid level
+            return 2;
+        } else {
+            // 80% chance for low level
+            return 1;
+        }
+    }
+
+    function withdraw() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+}

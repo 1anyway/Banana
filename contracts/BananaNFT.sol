66dde8d822a94e8d14e980be593f9e9e17b8ad86
周哲 <1708834102@qq.com>
2024-07-03 16:42:16 +0800

    update

--- contracts/BananaNFT.sol
+++ contracts/BananaNFT.sol
@@ -5,12 +5,9 @@ pragma solidity 0.8.26;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 contract BananaNFT is ERC721Enumerable, Ownable {
-    using Strings for uint256;
 
     uint256 private _tokenIdCounter;
 

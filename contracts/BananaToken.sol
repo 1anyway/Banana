10eccc437cfe76a11898601cc642667e6a2e6eb3
周哲 <1708834102@qq.com>
2024-07-03 13:49:48 +0800

    fix bug

--- contracts/BananaToken.sol
+++ contracts/BananaToken.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.26;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BananaToken is ERC20, Ownable {
     constructor(uint256 initialSupply) ERC20("BananaToken", "BANANA") Ownable(msg.sender) {
---
+++ scripts/BananaToken.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import {BananaToken} from "../contracts/BananaToken.sol";
+
+contract BananaTokenScript is Script {
+    function run() public {
+        vm.broadcast();
+        new BananaToken(1e26);
+    }
+}
